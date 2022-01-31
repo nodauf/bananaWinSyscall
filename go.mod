30f76590f8edf189cf09c8b99719bad0699a86d0
Nodauf <nodauf@krutt.org>
2022-01-31 17:34:09 +0400

    add NtOpenProcess

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nodauf/bananaWinSyscall
+
+go 1.17
+
+require golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27
---
+++ syscall.go
@@ -0,0 +1,30 @@
+package main
+
+import "golang.org/x/sys/windows"
+
+type ClientID struct {
+	UniqueProcess windows.Handle
+	UniqueThread  windows.Handle
+}
+
+/*
+__kernel_entry NTSYSCALLAPI NTSTATUS NtAllocateVirtualMemory(
+  HANDLE    ProcessHandle,
+  PVOID     *BaseAddress,
+  ULONG_PTR ZeroBits,
+  PSIZE_T   RegionSize,
+  ULONG     AllocationType,
+  ULONG     Protect
+);
+*/
+//dsys NtAllocateVirtualMemory(processHandle windows.Handle, baseAddress *uintptr, zeroBits uintptr, regionSize *uintptr, allocationType uint64, protect uint64) (err error)
+
+/*__kernel_entry NTSYSCALLAPI NTSTATUS NtOpenProcess(
+  [out]          PHANDLE            ProcessHandle,
+  [in]           ACCESS_MASK        DesiredAccess,
+  [in]           POBJECT_ATTRIBUTES ObjectAttributes,
+  [in, optional] PCLIENT_ID         ClientId
+);*/
+//dsys NtOpenProcess(processHandle windows.Handle, desiredAccess windows.ACCESS_MASK, objectAttributes *windows.OBJECT_ATTRIBUTES, clientID *ClientID) (err error)
+
+//go:generate go run github.com/nodauf/bananaWinSyscall/mkdirectwinsyscall -output zsyscall_windows.go syscall.go
